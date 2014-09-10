d71d12401186744ffecb5f7d44ffe1a8c17f6364
simonrakovic <simonrakovic@gmail.com>
2014-09-10 11:22:55 +0200

    init

---
+++ webpage/urls.py
@@ -0,0 +1,13 @@
+__author__ = 'user'
+
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+
+    url(r'^home/', 'webpage.views.home', name='home'),
+
+                       )
