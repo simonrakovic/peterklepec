c5d2197cefe1ccfda848a887f3bd5cf16503f470
simonrakovic <simonrakovic@gmail.com>
2014-10-10 00:30:48 +0200

    popravki

--- peterklepec_webpage/settings.py
+++ peterklepec_webpage/settings.py
@@ -184,7 +184,10 @@ import dj_database_url
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 DATABASES = {'default': dj_database_url.parse('sqlite:///'+PROJECT_PATH+'db.sqlite3')}
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-ALLOWED_HOSTS = ['localhost:8000']
+ALLOWED_HOSTS = [
+    'localhost:8000',
+    '0.0.0.0:32432/'
+]
 
 
 STATIC_ROOT = 'staticfiles'
