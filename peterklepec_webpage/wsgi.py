a1f015d7f3fb3930a923ffb8b535e5788615de0f
simonrakovic <simonrakovic@gmail.com>
2014-11-01 11:20:21 +0100

    dodani static fiajli za django admin

--- peterklepec_webpage/wsgi.py
+++ peterklepec_webpage/wsgi.py
@@ -24,13 +24,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "peterklepec_webpage.settings-se
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
-
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
+from django.core.handlers.wsgi import WSGIHandler
+application = WSGIHandler()
